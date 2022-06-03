d586ee4f7e27e892ad112cf1d0a20b3ff6482ad1
karthik reddy <karthik.kun009@gmail.com>
2022-06-03 17:44:47 +0530

    landing page | header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,12 @@ import '../css/Header.css'
 
 function Header() {
   return (
-    <div>
-        Header
+    <div className='header-section'>
+        <div className="main-section">
+          <span className='main-section-title'>Listing Application</span>
+          <span className='main-section-subtitle'>Listings</span>
+        </div>
+        <img className='main-image' src="https://images.pexels.com/photos/2931251/pexels-photo-2931251.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
     </div>
   )
 }
